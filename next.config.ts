0c06e7debc96f62201fb07d78f600328e1f7cfc0
Piotr <hunter976@gmail.com>
2025-07-28 19:58:55 +0200

    added nextjs github pages things

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Required for GitHub Pages static export
     domains: ["avatars.githubusercontent.com"],
     // Or for more advanced control (Next.js 13+):
     // remotePatterns: [
@@ -12,6 +15,9 @@ const nextConfig: NextConfig = {
     //   },
     // ],
   },
+  assetPrefix: isProd ? "/user-search/" : "",
+  basePath: isProd ? "/user-search/" : "",
+  output: "export",
 };
 
 export default nextConfig;
